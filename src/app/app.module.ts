802d508ecaff47d20fb76d050aabd8058ff4dd01
bryanst18 <brayansomz96@hotmail.com>
2020-09-10 10:23:02 -0500

    Creacion de 6 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavigationComponent } from './componentes/navigation/navigation.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { PrivateComponent } from './componentes/private/private.component';
+import { ErrorPageComponent } from './componentes/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegisterComponent,
+    PrivateComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
