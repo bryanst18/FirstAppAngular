c21b6bbf4ed879b94a460e9fc21bd5e327885d24
bryanst18 <brayansomz96@hotmail.com>
2020-09-12 04:15:51 -0500

    Se asignaron las rutas a cada componente menos navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from './componentes/home/home.component';
+import {ErrorPageComponent} from './componentes/error-page/error-page.component';
+import {LoginComponent} from './componentes/login/login.component';
+import {PrivateComponent} from './componentes/private/private.component';
+import {RegisterComponent } from './componentes/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'privado', component: PrivateComponent},
+  {path:'registro', component: RegisterComponent},
+  {path:'**', component: ErrorPageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
